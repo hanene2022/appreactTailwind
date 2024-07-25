ed589e273d0b5738f7d648436de984c42b929d25
hanenedevfull <hanenedevfulljs@gmail.com>
2024-07-25 16:47:37 +0200

    section_getStarted

---
+++ src/components/GetStarted.jsx
@@ -0,0 +1,47 @@
+
+function GetStarted() {
+  return (
+    <section>
+<div className="container relative">
+           
+<div className="bg-[#1c2230] absolute 
+left-[50%] translate-x-[-50%] top-[-150px] rounded-[5px] element-center
+ flex-col w-[865px]
+  max-w-full 
+  min-h-[275px]
+  text-white text-center p-[30px]">
+<h3 className="text-[25px]
+md:text-[35px]
+font-semibold mb-[15px]
+
+">
+
+
+
+
+    Get early access today</h3>
+    <p className="w-[620px] max-w-full mx-auto mb-[30px]">Lorem ipsum dolor sit amet 
+        consectetur adipisicing elit. 
+        Minima aspernatur quos debitis
+         nihil, facilis harum soluta tenetur cumque 
+         officiis sed? Quia nam magnam sunt ad perspiciatis deserunt molestias voluptate corrupti.
+
+
+    </p>
+    <form className=" w-full
+     flex justify-between 
+     items-center
+      flex-wrap 
+      md:px-[60px] gap-[30px]">
+        <input type="email"  className = 
+        "w-full md:flex-1  h-[50px] rounded-[30px] pl-[30px] outline-none border-none text-sm text-black font-medium"  
+         placeholder="email@exemple.com"/>
+   <button type="submit" className="w-full  md:w-[200px] h-[50px]bg-[#42b0d1]hover:bg-[#8cdae4] transition-all duration-200 rounded-[30px] ">Get Started for Free</button>
+    </form>
+</div>
+</div>
+</section>
+  )
+}
+
+export default GetStarted
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,7 @@ import Landing from "../components/Landing"
 import Features from "../components/Features";
 import StayProductive from "../components/StayProductive";
 import Testimonials from "../components/Testimonials";
+import GetStarted from "../components/GetStarted";
 
 
 function Home() {
@@ -12,6 +13,7 @@ function Home() {
       <Features />
       <StayProductive />
       <Testimonials />
+      <GetStarted />
     </Fragment>
   )
 }
